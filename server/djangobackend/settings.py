79bdf4dd1d6370249bbc9eee53c9c9d8ea5eae7c
GENPBNJ <45128565+GENPBNJ@users.noreply.github.com>
2022-02-18 18:46:55 -0600

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "tjablenski.us-south.cf.appdomain.cloud"]
+ALLOWED_HOSTS = ["localhost", "tjablenski.us-south.cf.appdomain.cloud", "tjablenskidjangoapp.us-south.cf.appdomain.cloud"]
 
 
 # Application definition
